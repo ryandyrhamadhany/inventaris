c1c584e05032bee16012596ed301319feceabffc
ryandyrhamadhany <ryandyrhamadhany@gmail.com>
2024-11-02 09:44:18 +0700

    update code laravel

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/admin', [AdminController::class, 'admin']);
