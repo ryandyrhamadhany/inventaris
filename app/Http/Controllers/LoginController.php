c66c0cb540e8cf49e839422fa301b06b93d129d5
ryandyrhamadhany <ryandyrhamadhany@gmail.com>
2024-11-09 09:51:46 +0700

    update code

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('login'); // Sesuaikan dengan nama file view login Anda
+    }
+
+    public function authenticate(Request $request)
+    {
+        // Validasi input login
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        // Coba autentikasi user
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+            // Jika login berhasil, arahkan ke halaman dashboard atau halaman utama
+            return redirect()->intended('/admin/beranda');
+        }
+
+        // Jika login gagal, kembalikan ke halaman login dengan pesan error
+        return back()->withErrors([
+            'username' => 'Username atau password salah.',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LaporanController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/admin', [AdminController::class, 'admin']);
+
+Route::get('/admin/beranda', [AdminController::class, 'admin'])->middleware('auth');
+Route::get('/admin/barang', [AdminController::class, 'barang']); //ini untuk menampilkan data
+Route::get('/admin/barang/tambah', [AdminController::class, 'tambah_barang']); //ini untuk menampilkan form tambah
+Route::get('/admin/barang/edit/{id}', [AdminController::class, 'edit_barang']); //ini untuk menampilkan form edit
+Route::get('/admin/barang/delete/{id}', [AdminController::class, 'delete_barang']); //ini untuk menghapus data
+Route::post('/admin/barang/simpan', [AdminController::class, 'simpan_barang']); //ini untuk menyimpan data
+Route::post('/admin/barang/update/{id}', [AdminController::class, 'update_barang']); //ini untuk mengupdate data
+
+Route::get('/admin/laporan', [LaporanController::class, 'index']); // ini untuk menampilkan halaman laporan
+Route::get('/admin/laporan/barang', [LaporanController::class, 'cetak_barang']); // ini untuk cetak barang
+
+Route::get('/kasir', function () {
+    return view('layouts.master');
 });
